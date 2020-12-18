82ebfa3dcd72e6e66cdd9a0db5accf292d822788
grocyshopandroiddev <58062625+grocyshopandroiddev@users.noreply.github.com>
2020-12-18 18:34:39 -0500

    I am just updated by a syso statement and coming back

--- src/test/java/Git_Project/First/MyClass.java
+++ src/test/java/Git_Project/First/MyClass.java
@@ -1,10 +1,11 @@
 package Git_Project.First;
 
 public class MyClass {
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+System.out.println("I am in master branch and got updated");
 	}
 
 }
